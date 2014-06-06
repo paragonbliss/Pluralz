37ef586f70450405d130a32d505a31240e1c9a5d
Michael Brandt <morbidfukk@gmail.com>
2014-06-06 05:20:19 +0200

    Init Commit0

---
+++ Brocfile.js
@@ -0,0 +1,47 @@
+/* global require, module */
+
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
+
+var app = new EmberApp({
+  name: require('./package.json').name,
+
+  minifyCSS: {
+    enabled: true,
+    options: {}
+  },
+
+  getEnvJSON: require('./config/environment')
+});
+
+// Style Assets
+app.import('vendor/bootstrap/dist/css/bootstrap.css');
+
+
+
+// Use this to add additional libraries to the generated output files.
+app.import('vendor/ember-data/ember-data.js');
+
+// If the library that you are including contains AMD or ES6 modules that
+// you would like to import into your application please specify an
+// object with the list of modules as keys along with the exports of each
+// module as its value.
+app.import('vendor/ic-ajax/dist/named-amd/main.js', {
+  'ic-ajax': [
+    'default',
+    'defineFixture',
+    'lookupFixture',
+    'raw',
+    'request',
+  ]
+});
+
+app.import('vendor/moment/moment.js');
+
+var extraAssets = pickFiles('vendor/bootstrap/dist/fonts', {
+  srcDir: '/',
+  files: ['**/*'],
+  destDir: '/fonts'
+});
+module.exports = mergeTrees([app.toTree(), extraAssets]);
